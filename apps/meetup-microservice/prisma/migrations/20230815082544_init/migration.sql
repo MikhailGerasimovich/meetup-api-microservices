45c15d9157e3dabb2f14e99ab28425907c154bfa
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-08-15 11:32:47 +0300

    Create prisma db model for meetup-microservice

---
+++ apps/meetup-microservice/prisma/migrations/20230815082544_init/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "Meetups" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "palce" TEXT NOT NULL,
+    "organizerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Meetups_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tags" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Tags_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MeetupsToTags" (
+    "id" SERIAL NOT NULL,
+    "meetupId" INTEGER NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "MeetupsToTags_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Members" (
+    "id" SERIAL NOT NULL,
+    "meetupId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Meetups_id_key" ON "Meetups"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tags_id_key" ON "Tags"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tags_title_key" ON "Tags"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MeetupsToTags_id_key" ON "MeetupsToTags"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Members_id_key" ON "Members"("id");
+
+-- AddForeignKey
+ALTER TABLE "MeetupsToTags" ADD CONSTRAINT "MeetupsToTags_meetupId_fkey" FOREIGN KEY ("meetupId") REFERENCES "Meetups"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MeetupsToTags" ADD CONSTRAINT "MeetupsToTags_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tags"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Members" ADD CONSTRAINT "Members_meetupId_fkey" FOREIGN KEY ("meetupId") REFERENCES "Meetups"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
