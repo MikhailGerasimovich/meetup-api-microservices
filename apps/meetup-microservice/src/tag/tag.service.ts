f70d4b2dd60e5b7a7ad690fec578358a5ef7dcf3
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-08-16 17:46:45 +0300

    update tag service

--- apps/meetup-microservice/src/tag/tag.service.ts
+++ apps/meetup-microservice/src/tag/tag.service.ts
@@ -1,35 +1,44 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { TagRepository } from './tag.repository';
-import { Tags } from '@prisma/client';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './types/tag.entity';
+import { TagCreationAttrs } from './types/tag.creation-attrs';
+import { TagUpdateAttrs } from './types/tag.update-attrs';
 
 @Injectable()
 export class TagService {
   constructor(private readonly tagRepository: TagRepository) {}
 
-  async readAll(): Promise<Tags[]> {
+  async readAll(): Promise<Tag[]> {
     const tags = await this.tagRepository.readAll();
     return tags;
   }
 
-  async readById(id: string): Promise<Tags> {
+  async readById(id: string): Promise<Tag> {
     const tag = await this.tagRepository.readById(id);
     return tag;
   }
 
-  async create(createTagDto: CreateTagDto): Promise<Tags> {
-    const createdTag = await this.tagRepository.create(createTagDto);
+  async create(createTagDto: CreateTagDto): Promise<Tag> {
+    const tagCreationAttrs: TagCreationAttrs = {
+      title: createTagDto.title,
+    };
+    const createdTag = await this.tagRepository.create(tagCreationAttrs);
     return createdTag;
   }
 
-  async update(id: string, updateTagDto: UpdateTagDto): Promise<Tags> {
+  async update(id: string, updateTagDto: UpdateTagDto): Promise<Tag> {
     const existingTag = await this.tagRepository.readById(id);
     if (!existingTag) {
       throw new BadRequestException(`The specified tag does not exist`);
     }
 
-    const updatedTag = await this.tagRepository.update(id, updateTagDto);
+    const tagUpdateAttrs: TagUpdateAttrs = {
+      title: updateTagDto.title || existingTag.title,
+    };
+
+    const updatedTag = await this.tagRepository.update(id, tagUpdateAttrs);
     return updatedTag;
   }
 
