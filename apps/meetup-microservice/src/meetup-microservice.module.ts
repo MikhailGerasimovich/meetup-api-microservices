be806ccfc7ab5d8c8233cc85b5e01805512d915a
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-08-18 12:56:05 +0300

    connect AllExceptionsFilter

--- apps/meetup-microservice/src/meetup-microservice.module.ts
+++ apps/meetup-microservice/src/meetup-microservice.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TagModule } from './tag/tag.module';
 import { MeetupModule } from './meetup/meetup.module';
+import { AllExceptionsFilter } from '@app/common';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [TagModule, MeetupModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class MeetupMicroserviceModule {}
